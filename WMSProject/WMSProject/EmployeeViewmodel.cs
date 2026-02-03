b8317c09dd29a51acf8ca87435cdc2671cc28a80
SzaZoa1227 <116081320+SzaZoa1227@users.noreply.github.com>
2026-02-03 10:40:40 +0100

    Change SzabadnapokSzama type from ushort to byte
    Updated the SzabadnapokSzama property in EmployeeViewmodel to use byte instead of ushort, reducing its value range to 0-255. This change may help optimize memory usage if higher values are not needed.

--- WMSProject/WMSProject/EmployeeViewmodel.cs
+++ WMSProject/WMSProject/EmployeeViewmodel.cs
@@ -4,7 +4,7 @@
     {
         public string Nev { get; } = "Nagy Anna";
         public string Adoszam { get; } = "1234567890-123";
-        public ushort SzabadnapokSzama { get; } = 15;
+        public byte SzabadnapokSzama { get; } = 15;
         public EmployeeViewmodel()
         {
         }
