a4b397d4604d5b83ce00f40bbda2e7f5ca6e8ea6
SzaZoa1227 <116081320+SzaZoa1227@users.noreply.github.com>
2026-02-03 10:37:20 +0100

    Add tax number and vacation days to employee view
    Added Adoszam (tax number) and SzabadnapokSzama (vacation days) properties to EmployeeViewmodel with default values. Updated MainWindow.xaml to display these new fields alongside the employee's name.

--- WMSProject/WMSProject/EmployeeViewmodel.cs
+++ WMSProject/WMSProject/EmployeeViewmodel.cs
@@ -3,6 +3,8 @@
     public class EmployeeViewmodel
     {
         public string Nev { get; } = "Nagy Anna";
+        public string Adoszam { get; } = "1234567890-123";
+        public ushort SzabadnapokSzama { get; } = 15;
         public EmployeeViewmodel()
         {
         }
